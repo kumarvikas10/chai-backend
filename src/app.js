f5eb029b831d88c538c107721b3e27a759a5d236
kumarvikas10 <vikasverma19960226@gmail.com>
2024-08-13 21:40:44 +0530

    Utility code

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+
+const app = express()
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+}))
+
+app.use(express.json({limit: "16kb"})) //json appceptance limit
+app.use(express.urlencoded({extended: true, limit: "16kb"})) //when url convert url into special chrachter then it is use url ecoder
+app.use(express.static("public")) //use to store file or folder or pdf
+app.use(cookieParser()) //use to read cookies and store cookies to user through server
+
+
+export {app} 
---
+++ src/index.js
@@ -0,0 +1,39 @@
+import { config as configDotenv } from "dotenv";
+import connectDB from "./db/index.js"; 
+import express from "express"
+const app = express()
+
+configDotenv({
+    path: './.env'
+});
+
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`server is running at port : ${process.env.PORT}`)
+    });
+})
+.catch((err)=> {
+    console.log("Mongo DB Connection Failed", err)
+})
+
+
+/*
+First Approch
+(async()=>{
+    try{
+        await mongoose.connect(`${process.env.MOGODB_URI}/${DB_NAME}`)
+        app.on("error", (error)=>{
+            comsole.log("Error: ", error);
+            throw err
+        })
+        app.listen(process.env.PORT, ()=>{
+            comsole.log(`App is listening on port ${process.env.PORT}`);
+        })
+    }catch(error){
+        comsole.log("Error: ", error);
+        throw err
+    }
+})()
+
+*/
